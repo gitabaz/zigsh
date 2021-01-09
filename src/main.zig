96ed5132bb964b0cf232d62f52fd22a0b13c571b
Aidan Zabalo <olabaz@gmail.com>
2021-01-09 15:45:43 -0500

    Add basic read from stdin

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
+    try stdout.print("zsh$ ", .{});
+
+    var buf: [100]u8 = undefined;
+    const bytes_read: anyerror!usize = stdin.read(&buf);
 }
